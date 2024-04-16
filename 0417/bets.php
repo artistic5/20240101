4724f537aee6736f572a24827ba977dc149804ba
artistic5 <artisticsoftware5@gmail.com>
2024-04-16 12:26:23 +0000

    ...

--- 0417/bets.php
+++ 0417/bets.php
@@ -29,11 +29,11 @@ return [
 		/**
 		Race 4
 		*/
-		'favorites' => '6, 11',
+		'favorites' => '2, 6, 11',
 		'Favs 6, 10' => 'true',
 		'Favs 11, 1' => 'true',
 		'Favs 11, 7' => 'true',
-		'win hist' => '4, 5, 6, 10, 11, 12',//count: 6
+		'win hist' => '1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12',//count: 11
 		'shit' => '1, 6, 7, 10, 11',
 	],
 	'5' => [
