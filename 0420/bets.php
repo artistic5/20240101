7721dcbb681d25252260cfb9b6ec71ca9d8b5e26
artistic5 <artisticsoftware5@gmail.com>
2024-04-19 05:40:08 +0000

    ...

--- 0420/bets.php
+++ 0420/bets.php
@@ -28,8 +28,10 @@ return [
 		/**
 		Race 4
 		*/
-		'favorites' => '12',
-		'win hist' => '1, 3, 11, 12',//count: 4
+		'favorites' => '5, 12',
+		'Favs 5, 5' => 'true',
+		'win hist' => '1, 2, 3, 4, 5, 7, 10, 11, 12',//count: 9
+		'shit' => '5',
 	],
 	'5' => [
 		/**
