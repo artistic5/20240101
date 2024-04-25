7c4415a6fedd9c0e8c885cba33771a52d5f29a39
artistic5 <artisticsoftware5@gmail.com>
2024-04-25 03:13:16 +0000

    ...

--- archive/0424/bets.php
+++ archive/0424/bets.php
@@ -13,7 +13,6 @@ return [
 		Race 2
 		*/
 		'favorites' => '1, 6, 7',
-		'inter' => '',
 	],
 	'3' => [
 		/**
@@ -21,46 +20,48 @@ return [
 		*/
 		'favorites' => '3, 7, 9',
 		'inter' => '3, 7',
+		'win/qin/trio' => '3, 7, 9',
 	],
 	'4' => [
 		/**
 		Race 4
 		*/
 		'favorites' => '1, 5, 11',
-		'inter' => '',
+		'inter' => '5',
 	],
 	'5' => [
 		/**
 		Race 5
 		*/
 		'favorites' => '1, 4, 5, 7, 9',
-		'inter' => '1, 4',
+		'inter' => '1, 4, 5',
+		'win/qin/trio' => '1, 4, 5, 7, 9',
 	],
 	'6' => [
 		/**
 		Race 6
 		*/
 		'favorites' => '4, 10',
-		'inter' => '',
 	],
 	'7' => [
 		/**
 		Race 7
 		*/
 		'favorites' => '2, 7, 12',
+		'inter' => '2',
 	],
 	'8' => [
 		/**
 		Race 8
 		*/
 		'favorites' => '1, 4, 8',
-		'inter' => '1',
+		'inter' => '1, 4',
+		'win/qin/trio' => '1, 4, 8',
 	],
 	'9' => [
 		/**
 		Race 9
 		*/
 		'favorites' => '1, 3, 4',
-		'inter' => '',
 	],
 ];
