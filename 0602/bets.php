cee0c15c0b956e8ac3b0bc80da264e26a6643314
artistic5 <artisticsoftware5@gmail.com>
2024-06-01 04:32:32 +0000

    ...

---
+++ 0602/bets.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '13',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2',
+		'inter' => '2',//count: 1
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '12',
+		'inter' => '12',//count: 1
+		'Place' => '12',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '10',
+		'inter' => '10',//count: 1
+		'Place' => '10',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '13',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '2',
+		'inter' => '2',//count: 1
+		'Place' => '2',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '5',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '7',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '2',
+		'inter' => '2',//count: 1
+		'Place' => '2',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '13',
+		'inter' => '13',//count: 1
+	],
+	'11' => [
+		/**
+		Race 11
+		*/
+		'favorites' => '13',
+	],
+];
+//total place: 0
+//total: 0
