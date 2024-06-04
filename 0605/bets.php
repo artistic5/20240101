7464276dcd8b3684abaff971cd4cdc410b6adbfa
artistic5 <artisticsoftware5@gmail.com>
2024-06-04 04:36:32 +0000

    ...

---
+++ 0605/bets.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '5',
+		'inter' => '5',//count: 1
+		'Place' => '5',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2',
+		'inter' => '2',//count: 1
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '4',
+		'inter' => '4',//count: 1
+		'Place' => '4',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '7',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '12',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '11',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '11',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '6',
+		'inter' => '6',//count: 1
+		'Place' => '6',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '11',
+	],
+];
+//total place: 0
+//total: 0
---
+++ 0605/indication.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '5',
+		'indication' => [],
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '2',
+		'indication' => [],
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '4',
+		'indication' => [],
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '7',
+		'indication' => [],
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '12',
+		'indication' => [],
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '11',
+		'indication' => [],
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '11',
+		'indication' => [],
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '6',
+		'indication' => [],
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '11',
+		'indication' => [],
+	],
+];
