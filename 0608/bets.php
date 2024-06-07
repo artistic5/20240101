f3ba59c0039b253a0d253cb032841b9afbc4d497
artistic5 <artisticsoftware5@gmail.com>
2024-06-07 05:18:12 +0000

    ...

---
+++ 0608/bets.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '14',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '7',
+		'inter' => '7',//count: 1
+		'Place' => '7',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2',
+		'inter' => '2',//count: 1
+		'Place' => '2',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1',
+		'inter' => '1',//count: 1
+		'Place' => '1',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '10',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '4',
+		'inter' => '4',//count: 1
+		'Place' => '4',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '7',
+		'inter' => '7',//count: 1
+		'Place' => '7',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '1',
+		'inter' => '1',//count: 1
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '7',
+		'inter' => '7',//count: 1
+	],
+];
+//total win: 0
+//total place: 0
+//total qin: 0
+//total trio: 0
+//total: 0
---
+++ 0608/indication.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '14',
+		'indication' => [],
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '7',
+		'indication' => [],
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '2',
+		'indication' => [],
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '1',
+		'indication' => [],
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '10',
+		'indication' => [],
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '4',
+		'indication' => [],
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '7',
+		'indication' => [],
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '2',
+		'indication' => [],
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '1',
+		'indication' => [],
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '7',
+		'indication' => [],
+	],
+];
